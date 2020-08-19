f167f1ee92eae1c55d5705a4f3216e7988831757
yuanmingyang <13564890705@163.com>
2020-08-19 10:14:55 +0100

    update idx_dict

--- src/train.py
+++ src/train.py
@@ -20,14 +20,13 @@ def train(args, data, show_loss, show_topk):
 
     # pre-process multi-clusters
     group_ids = list(range(math.ceil(args.num_clusters / args.block_size)))
+    # args.batch_size = math.ceil(args.batch_size / len(group_ids))
     multi_adj_entities, multi_adj_relations, train_data_multi_map, eval_data_multi_map, test_data_multi_map = \
         partition_utils.preprocess_multicluster(adj, kg, idx_nodes, groups, train_ord_map, args.num_clusters,
                                                 args.block_size, args.neighbor_sample_size,
                                                 train_data, eval_data, test_data,
                                                 train_item_idx_dict, eval_item_idx_dict, test_item_idx_dict,
                                                 train_item_next_dict, eval_item_next_dict, test_item_next_dict)
-
-    # model = KGCN(args, n_user, n_entity, n_relation, total_adj_entities, total_adj_relations)
 
     # Some preprocessing
     model_func = models.KGCN
